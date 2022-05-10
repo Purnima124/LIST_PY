d04bf603e19184fcb356d84a74c31b9639475e90
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-10 23:14:11 +0530

    Add files via upload

---
+++ I.N question,24 apirl.py
@@ -0,0 +1,33 @@
+# a=["12","14","15","18","21"]
+# b=["a","b","c","d","e"]
+# i=0
+# yn=[]
+# while i<len(a):
+#     x=b[0]+a[0],b[1]+a[1],b[2]+a[2],b[3]+a[3],b[4]+a[4]
+#     yn.append(x)
+#     i=i+1
+#     break
+# print(yn)
+
+# a=["12","14","15","18","21"]
+# b=["a","b","c","d","e"]
+# i=0
+# y=[]
+# # j=0
+# while i <len(a):
+#     p=b[i]+a[i]
+#     y.append(p)
+#     i=i+1
+# print(y)
+
+
+
+list1=["M","na","i","ke"]
+list2=["y","me","s","lly"]
+i=0
+y=[]
+while i<len(list1):
+    k=list1[i]+list2[i]
+    y.append(k)
+    i=i+1
+print(y)
