01146a529e5d056efc75cf26c3078eb10015dba2
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-10 23:14:32 +0530

    Add files via upload

---
+++ iterate question.py
@@ -0,0 +1,22 @@
+# a=["rishabh","madhurima","rahul","abhishek","faijal","muskan"]
+# # marks=[10,20,56,45,36,20]
+# # print (len(a))
+# # print (len(marks))
+# b=len(a)
+# counter=0
+# i=0
+# while counter <len(counter):
+#     print(student[counter]+str(marks[counter])
+
+num=[50,40,23,70,56,12,5,10,7]
+b=[]
+i=0
+count=0
+while i<len(num):
+    a=num[i]
+    if a>20 and a<40:
+            b.append(a)
+            count=count+1
+    i=i+1                
+print(b,"count=",count)
+
