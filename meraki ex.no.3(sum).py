c97a3aead7bd0395d233a4052d7a7debdc3a384b
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-10 23:17:29 +0530

    Add files via upload

---
+++ meraki ex.no.3(sum).py
@@ -0,0 +1,30 @@
+num=[50,40,30,70,23,25]
+i=0
+sum=0
+while i< len(num):
+    sum=sum+num[i]
+    i=i+1
+print(sum)
+
+
+
+# names_list="abhishek"
+# print (names_list[0] )
+# names_list="dhruv" 
+# print(names_list[0])
+
+# names_list = ["annu", "shivam", "deepa", "pooja", "rupa"]
+
+# print (names_list[0]) # se "annu" print hoga
+
+# print (names_list[4]) # se "rupa" print hoga
+
+# print (names_list[5]) 
+# a="abhishek"
+# print(a[3])
+
+
+
+
+
+
