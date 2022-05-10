f1b604b405e92ce789db7b757a3b6f058495d9ab
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-10 23:16:34 +0530

    Add files via upload

---
+++ list joiend karna.py
@@ -0,0 +1,19 @@
+# a=["m","na","i","pur"]
+# b=["y","me","s","nima"]
+# i=0
+# yn=[]
+# while i<len(a):
+#     x=a[0]+b[0],a[1]+b[1],a[2]+b[2],a[3]+b[3]
+#     yn.append(x)
+#     i=i+1
+# print(yn)
+a=["12","14","15","18","21"]
+b=["a","b","c","d","e"]
+i=0
+yn=[]
+while i<len(a):
+    x=b[0]+a[0],b[1]+a[1],b[2]+a[2],b[3]+a[3],b[4]+a[4]
+    yn.append(x)
+    i=i+1
+    break
+print(yn)
