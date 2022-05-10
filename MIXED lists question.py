c3f603eb72eb0fec0101aad6453b0196e3d8654b
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-10 23:27:10 +0530

    Add files via upload

---
+++ MIXED lists question.py
@@ -0,0 +1,8 @@
+mixed_list = ["rahul", 12, 9.0, "kaavay", "shivam", 1]
+print(mixed_list[-4]) 
+names_list="rishabh"
+print (names_list[-3])
+
+names_list="alok"
+# print (names_list)
+print (names_list[2]) 
