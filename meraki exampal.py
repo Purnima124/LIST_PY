8ea1b112056e79392275ff96c608228250575936
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-10 23:20:10 +0530

    Add files via upload

---
+++ meraki exampal.py
@@ -0,0 +1,21 @@
+# a=["annu","shivam","deepa","pooja","rupa"]
+# print(len(a))
+# print(a)
+# a=[50,40,23,70,56,12,5,10,7]
+# print(len(a))
+# print(a)
+
+a=[7,8,14,13,11,70]
+i=0
+y=[]
+# num=0
+while i<len(a):
+    k=a[i]
+    if k%7==0:
+        y.append(k)
+    i=i+1    
+print(y)    
+        
+    
+        
+    
