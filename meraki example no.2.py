709cec4fa30fe531c9733736fa2ffca4742ed388
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-10 23:26:34 +0530

    Add files via upload

---
+++ meraki example no.2.py
@@ -0,0 +1,7 @@
+names_list = ["annu", "shivam", "deepa", "pooja", "rupa"]
+print (names_list[-0])
+print (names_list[-1])
+print (names_list[-2])
+print (names_list[-3])
+print (names_list[-4])
+print (names_list[-5])
