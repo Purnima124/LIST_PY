aab7f6ed621e3126c61dcc11a861e4ad1da09a30
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-10 23:21:55 +0530

    Add files via upload

---
+++ meraki exampale question.py
@@ -0,0 +1,13 @@
+a=["robin","anamika","faisal","valmii","waseem","amra"]
+b=len(a)
+i=0
+while i<b:
+    print(a[i])
+    i=i+1
+ 
+# students= ["robin", "anamika", "faisal", "valmiki", "waseem", "amara"]
+# list=len(students)
+# index=0
+# while index<list:
+#     print(students[index])
+#     index=index+1 
