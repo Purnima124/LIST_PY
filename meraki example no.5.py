6810b55738eaf321c6bb39b9f940b37761f6d6f4
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-10 23:26:50 +0530

    Add files via upload

---
+++ meraki example no.5.py
@@ -0,0 +1,38 @@
+num=[50,40,23,70,56,12,5,10,7]
+i=0
+c=0
+while i<len(num):
+    c=c+1
+    i=i+1
+print(num,"count",c)
+   
+   
+# a=num[0]
+# while i<len(num):
+#     if num[i]>a:
+#         a=num[i]
+#         # i=i+1
+#     print(a)
+#     i=i+1
+# num=[11,12,13]
+# b=max(num) 
+# print(b)
+# num=[11,12,13]
+# i=0
+# a=num[i]
+# while i<(num):
+#     if num[i]>a:
+#         a=num[i]
+#     print(a)
+#     i=i+1
+
+# num=[50, 40, 23, 70, 56, 12, 5, 10, 7]
+# i=0
+# b=num[0]
+# while i<len(num):
+#     if num[i]>b:
+#         b=num[i]
+#     i=i+1
+# print(b)
+
+    
