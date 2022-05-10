5bc91aa0cea74beebb3584cc3c550c4954aea0ab
Purnima124 <82706192+Purnima124@users.noreply.github.com>
2022-05-10 23:21:27 +0530

    Add files via upload

---
+++ meraki exampale.py
@@ -0,0 +1,31 @@
+# a=[23,45,89,90,56,80]
+# i=0
+# sum=0
+# while i<len(a):
+#     sum=sum+a[i]
+#     i=i+1
+# print(sum)
+
+
+
+         
+student_marks=[23, 45, 67, 89, 90, 54, 34, 21, 34, 23, 19, 28, 10, 45, 86, 87, 9]
+list_length=len(student_marks)
+index=0
+less_than50=0
+more_than50=0
+while index<list_length:
+    marks=student_marks[index]
+    if marks<50:
+        less_than50=less_than50+1
+    else:
+        more_than50=more_than50+1
+    index=index+1
+print("Marks more than 50:"+str(more_than50))
+print("Marks less than 50:"+str(less_than50))
+
+
+
+
+
+
